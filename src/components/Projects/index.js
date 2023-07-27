fffc152b21bfe6b71c8d74c96a8c0dea1ef12a5c
Johongirr <johongir_rahimov@bk.ru>
2023-07-27 09:26:13 +0500

    fix error

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -94,62 +94,6 @@ const Projects = () => {
         whileInView="onscreen"
         viewport={{ once: true, amount: 0.8 }}
       >
-        <motion.div variants={scrollConfigs}>
-          <header>
-            <h3>{t("myProjects.bloodDonationApp.project1Title")}</h3>
-            <p>{t("myProjects.bloodDonationApp.project1Text1")}</p>
-            <p>{t("myProjects.bloodDonationApp.project1Text2")}</p>
-          </header>
-          <div>
-            <h4>{t("myProjects.bloodDonationApp.project1TechStack")}</h4>
-            <ul>
-              <li>Node.js</li>
-              <li>Express.js</li>
-              <li>Typescript</li>
-              <li>Mongoose</li>
-              <li>Jsonwebtoken</li>
-              <li>React</li>
-              <li>Redux</li>
-              <li>Formik</li>
-              <li>Yup</li>
-            </ul>
-          </div>
-          <ProjectCardLinkBox>
-            <a
-              href="https://github.com/Johongirmirzo/blood-donor"
-              target="_blank"
-              rel="noreferrer"
-              className="project-link"
-            >
-              <BsGithub />
-            </a>
-            <a
-              href="https://blood-donorr.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-              className="project-link"
-            >
-              <HiOutlineExternalLink />
-            </a>
-          </ProjectCardLinkBox>
-        </motion.div>
-        <motion.a
-          href="https://blood-donorr.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-          variants={scrollConfigs}
-        >
-          <img
-            src={BloodDonorImg}
-            alt="pic of the client side of the blood donation app"
-          />
-        </motion.a>
-      </ProjectCard>
-      <ProjectCard
-        initial="offscreen"
-        whileInView="onscreen"
-        viewport={{ once: true, amount: 0.8 }}
-      >
         <motion.a
           href="https://pharmacy-management-app-admin.netlify.app/login"
           target="_blank"
